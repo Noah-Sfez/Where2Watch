51f9afabc2bbc7e9d59d01240ee22be1e3a1d811
Noah Sfez <sfz.noah@gmail.com>
2024-04-16 18:13:29 +0200

    Ajout des premiers filtrages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,80 @@
 <template>
   <main>
-    <div class="search-container">
-      <input
-        type="text"
-        placeholder="Rechercher des films, séries, acteurs..."
-        v-model="searchQuery"
-        @input="searchMovies"
-      />
-    </div>
-    <div v-if="searchResults.length > 0">
-      <div v-for="(result, index) in searchResults" :key="result.id + (result.media_type || index)" class="search-result" @click="goToDetail(result)">
-        <img :src="getImageUrl(result)" :alt="result.title || result.name">
-        <h2>{{ result.title || result.name }}</h2>
+    <!-- Zone de recherche et filtres -->
+    <div class="search-and-filters">
+      <div class="search-container">
+        <input
+          type="text"
+          placeholder="Rechercher des films, séries, acteurs..."
+          v-model="searchQuery"
+          @input="searchContent"
+        />
       </div>
+      <!-- Filtres -->
+      <div class="filters">
+        <select v-model="selectedGenre" @change="applyFilters">
+          <option value="">Tous les genres</option>
+          <option v-for="genre in genres" :value="genre.id" :key="genre.id">{{ genre.name }}</option>
+        </select>
+        <select v-model="selectedRating" @change="applyFilters">
+          <option value="">Toutes les notes</option>
+          <option value="9">9 et plus</option>
+          <option value="8">8 et plus</option>
+          <option value="7">7 et plus</option>
+          <option value="6">6 et plus</option>
+          <option value="5">5 et plus</option>
+        </select>
+        <select v-model="selectedYear" @change="applyFilters">
+          <option value="">Toutes les années</option>
+          <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
+        </select>
+        <!--
+        <div v-for="service in streamingServices" :key="service.provider_id">
+          <input
+            type="checkbox"
+            :value="service.provider_id"
+            v-model="selectedStreamingServices"
+            @change="applyFilters"
+          >
+          <label>{{ service.provider_name }}</label>
+        </div>-->
+        <!-- Ajoute ici d'autres sélecteurs de filtre si nécessaire -->
+      </div>
+
     </div>
-    <div v-else>
-      <div v-for="(movie, index) in movies" :key="movie.id + 'movie-' + index" class="movie" @click="goToMovie(movie)">
-        <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" :alt="movie.title">
-        <h2>{{ movie.title }}</h2>
+    <!-- Résultats de la recherche ou films par défaut -->
+    <div class="content-grid">
+      <div v-for="(item, index) in displayMovies" :key="item.id + 'display-' + index" class="content-item movie" @click="goToMovie(item)">
+        <img :src="getImageUrl(item)" :alt="item.title || item.name">
+        <h2>{{ item.title || item.name }}</h2>
       </div>
     </div>
   </main>
 </template>
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 const searchQuery = ref('');
-const movies = ref([]);
+const movies = ref([]); // Assure-toi que ceci est initialisé à un tableau vide.
 const searchResults = ref([]);
+const selectedGenre = ref('');
+const filteredMovies = ref([]);
+const genres = ref([]);
+const streamingServices = ref([]);
+const popularStreamingServices = [
+  { id: 'Netflix_ID', name: 'Netflix' },
+  { id: 'AmazonPrime_ID', name: 'Amazon Prime' },
+  { id: 'Hulu_ID', name: 'Hulu' },
+];
+const selectedRating = ref('');
+const selectedYear = ref('');
+const years = ref([]);
+const selectedStreamingServices = ref([]);
 const router = useRouter();
 const apiKey = 'a446566ae142c41aa0fcfd2febbee065';
 const baseUrl = 'https://api.themoviedb.org/3';
+const displayMovies = computed(() => searchResults.value.length > 0 ? searchResults.value : filteredMovies.value.length > 0 ? filteredMovies.value : movies.value);
 async function loadMovies() {
   let allMovies = [];
   try {
@@ -41,12 +84,26 @@ async function loadMovies() {
     }
     const responsePage3 = await axios.get(`${baseUrl}/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=3`);
     allMovies = allMovies.concat(responsePage3.data.results.slice(0, 20));
-    movies.value = allMovies;
+    movies.value = allMovies.map(movie => ({ ...movie, media_type: 'movie' }));
   } catch (error) {
     console.error(error);
   }
 }
-async function searchMovies() {
+async function loadStreamingServices() {
+  try {
+    const response = await axios.get(`${baseUrl}/watch/providers/movie?api_key=${apiKey}&language=fr-FR`);
+    streamingServices.value = response.data.results;
+  } catch (error) {
+    console.error('Erreur lors du chargement des fournisseurs de streaming:', error);
+  }
+}
+onMounted(() => {
+  const currentYear = new Date().getFullYear();
+  for (let year = currentYear; year > currentYear - 30; year--) {
+    years.value.push(year);
+  }
+});
+async function searchContent() {
   if (searchQuery.value.length < 2) {
     searchResults.value = [];
     return;
@@ -58,24 +115,56 @@ async function searchMovies() {
     console.error(error);
   }
 }
-function goToDetail(result) {
-  if (result.media_type === 'movie') {
-    router.push({ name: 'MovieDetails', params: { id: result.id } });
-  } else if (result.media_type === 'tv') {
-    router.push({ name: 'TvShowDetails', params: { id: result.id } }); 
-  } else if (result.media_type === 'person') {
-    router.push({ name: 'ActorDetails', params: { id: result.id } });
+async function applyFilters() {
+  let url = `${baseUrl}/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false`;
+  if (selectedGenre.value) {
+    url += `&with_genres=${selectedGenre.value}`;
+  }
+  if (selectedRating.value) {
+    url += `&vote_average.gte=${selectedRating.value}`;
+  }
+  if (selectedYear.value) {
+    url += `&primary_release_year=${selectedYear.value}`;
+  }
+  if (selectedStreamingServices.value.length > 0) {
+    const providersIds = selectedStreamingServices.value.join('|'); // Utilise '|' comme séparateur si l'API le requiert
+    url += `&with_watch_providers=${providersIds}&watch_region=FR`;
+  }
+  try {
+    const response = await axios.get(url);
+    filteredMovies.value = response.data.results;
+  } catch (error) {
+    console.error("Erreur lors de l'application des filtres : ", error);
   }
 }
-function goToMovie(movie) {
-  router.push({ name: 'MovieDetails', params: { id: movie.id } });
+async function loadGenres() {
+  try {
+    const response = await axios.get(`${baseUrl}/genre/movie/list?api_key=${apiKey}&language=fr-FR`);
+    genres.value = response.data.genres;
+  } catch (error) {
+    console.error('Erreur lors du chargement des genres:', error);
+  }
+}
+function goToMovie(result) {
+  const mediaType = result.media_type || 'movie'; // Assume 'movie' if media_type is not provided
+  if (mediaType === 'movie') {
+    router.push({ name: 'MovieDetails', params: { id: result.id } });
+  } else if (mediaType === 'tv') {
+    router.push({ name: 'TvShowDetails', params: { id: result.id } });
+  } else if (mediaType === 'person') {
+    router.push({ name: 'ActorDetails', params: { id: result.id } });
+  }
 }
 function getImageUrl(result) {
   return result.poster_path || result.profile_path
     ? `https://image.tmdb.org/t/p/w500${result.poster_path || result.profile_path}`
     : ''; // You may want to return a placeholder image URL here
 }
-onMounted(loadMovies);
+onMounted(async () => {
+  await loadMovies();
+  await loadGenres();
+  await loadStreamingServices();
+});
 </script>
 <style>
 .movie {
